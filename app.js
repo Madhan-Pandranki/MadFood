2e1a35fd958aa183f588441ed5f9af19f96f8485
Madhan Pandranki <117290421+Madhanp0503@users.noreply.github.com>
2024-02-17 03:01:31 +0530

    NamsteFood

--- app.js
+++ app.js
@@ -1,59 +1,86 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// const heading = React.createElement("h1", {id : "heading"}, "Hello React from ReactJS");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
 
-// const parent = React.createElement("div", {id: "parent"}, [
-//     React.createElement("div", {id: "child1"}, [
-//         React.createElement("h1", {}, "Im h1 tag in child div"),
-//         React.createElement("h2", {}, "Im h2 tag in child div")
-//     ]),
-//     React.createElement("div", {id: "child2"}, [
-//         React.createElement("h1", {}, "Im h1 tag in child div"),
-//         React.createElement("h2", {}, "Im h2 tag in child div")
-//     ])
-// ])
-// console.log(parent) //object
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);    //replaces all elements in the tag of html
+/**
+ * Header
+ *  - Logo
+ *  - Nav items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *      - RestaurantCard
+ *          - Image
+ *          - Name, Rating, Cuisine, Delivery Time
+ * Footer
+ *  - Copyrights
+ *  - Links
+ *  - Address
+ *  - Contact info
+ */
 
-//* JSX developed by Facebook (different from react) -> HTML like syntax
-//TODO: transpiled before it reaches the JS -> PARCEL -> Babel(transpiler)
-//! JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-const ele = <span>React element</span>;
-//React Element
-const jsxheading = (
-  <h1 id="jsxheading" className="head">
-    {ele} {/* Element in an element*/}
-    Namaste react using JSX 💕
-  </h1>
-);
-
-//? React Components
-//// Class based components -> old
-// Functional components -> new
-const Title = () => {
+const Header = () => {
   return (
-    <h1 className="head" tabIndex="5">
-      Namaste React Title Component
-    </h1>
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://img.freepik.com/free-vector/delivery-service-illustrated_23-2148505081.jpg?w=740&t=st=1708115596~exp=1708116196~hmac=05352d32d85f9e8693c85e12d651a4d2505d7ac4b37a3ae1c454fe92231e9e29"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
-const number = 1000;
+const styleCard = {
+  // Inline CSS
+  backgroundColor: "#f0f0f0",
+};
+const RestaurantCard = (props) => {
+  console.log(props);
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cf6280d32aefb59cc849ef81b39fd3fd"
+      ></img>
+      <h3>{props.resname}</h3>
+      <h4>{props.cuisine}</h4>
+      <h4>4.3 stars</h4>
+      <h4>25 mins</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard resname="Madfood" cuisine="Pizza,Jalandhar,Punjab" />
+        <RestaurantCard
+          resname="KFC"
+          cuisine="Fried chicken,Jalandhar,Punjab"
+        />
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div id="container">
-    <Title /> {/*Component in a component*/}
-    <Title></Title>
-    {Title()}
-    {jsxheading} {/*Element in a component*/}
-    <h2>{number}</h2> {/* Using JS inside JSX */}
-    <h1>Namaste React From Heading Component</h1>
-  </div>
-);
-
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
